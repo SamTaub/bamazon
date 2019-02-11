bc2b6afeb841497c3b9ca585efcd43a88969db2d
Sam Taub <staub91@gmail.com>
2019-02-11 13:48:54 -0500

    added manager functionality, updated readme

--- README.md
+++ README.md
@@ -9,6 +9,7 @@ Bamazon is a mock marketplace that houses various items.  This application runs 
 Users can enter customer mode by typing "node bamazonCustomer" into the terminal.  Use "node bamazonManager" to use the application in manager mode.
 
 * Customer Mode
+    
     * When the application starts the Bamazon Inventory will be displayed in the terminal.
     * Customers will be prompted to "Purchase an item" or "Exit Bamazon".  Exiting Bamazon will close the application.
     * When purchasing an item, users will be required to enter a valid product ID and quantity of the desired item.
@@ -16,6 +17,19 @@ Users can enter customer mode by typing "node bamazonCustomer" into the terminal
     * Upon completion of purchase, customers will have the option to purcahse another item or exit Bamazon.
 
 * Manager Mode
+
+Manager Mode provides the user with an inquirer prompt with a list of options to view and manage the Bamazon inventory.
+
+* View items
+    * Allows the user to view all items and their stock that are in the Bamazon inventory.
+* View low inventory
+    * Provides the user with a list of products with a stock quantity of 5 or less.
+* Add to inventory
+    * Prompts the user to select an item and a quantity to add its stock.
+* Add new product
+    * Prompts the user to add a new item to the Bamazon Inventory.
+* Exit Bamazon
+    * Leaves the application and terminates the connection to the Bamazon Database.
 
 ### Technology Used
 
--- bamazonManager.js
+++ bamazonManager.js
@@ -19,30 +19,119 @@ connection.connect(function (err, res) {
     managerPrompt();
 });
 
-var managerPrompt = function() {
-inquirer.prompt({
-    name: "managerPrompt",
-    type: "list",
-    message: "Select an action",
-    choices: ["View items for sale", "View low inventory", "Add to inventory", "Add new product", "Exit Bamazon Manager"]
-}).then(function(answer){
-    
-    switch (answer.managerPrompt) {
-        case "View items for sale":
-        viewItems();
-        break;
-        case "View low inventory":
-        viewLow();
-        break;
-        case "Add to inventory":
-        addInventory();
-        break;
-        case "Add new product":
-        newProduct();
-        break;
-        default: 
-        console.log("Exiting Bamazon Manager...  Have a nice day!");
-        connection.end();
-    }
-})
+var managerPrompt = function () {
+    inquirer.prompt({
+        name: "managerPrompt",
+        type: "list",
+        message: "Select an action",
+        choices: ["View items for sale", "View low inventory", "Add to inventory", "Add new product", "Exit Bamazon Manager"]
+    }).then(function (answer) {
+        switch (answer.managerPrompt) {
+            case "View items for sale":
+                viewItems();
+                break;
+            case "View low inventory":
+                viewLow();
+                break;
+            case "Add to inventory":
+                addInventory();
+                break;
+            case "Add new product":
+                newProduct();
+                break;
+            default:
+                console.log("Exiting Bamazon Manager...  Have a nice day!");
+                connection.end();
+        }
+    })
+};
+
+var viewItems = function () {
+    //Selects all content from the Products table.  A for loop is used to display the data for all products.
+    var query = "SELECT * FROM products";
+    connection.query(query, function (err, res) {
+        if (err) throw err;
+        console.log("\nID | Product | Department | Price | Quantity")
+        for (var i = 0; i < res.length; i++) {
+            console.log("\n" + res[i].id, res[i].productName, res[i].departmentName, res[i].price, res[i].stockQuantity);
+        };
+        console.log("\n\n");
+        //After the Bamazon Inventory is displayed, "managerPrompt()" runs again to provide the user with further options.
+        managerPrompt();
+    });
+};
+
+var viewLow = function () {
+    connection.query("SELECT id, productName, stockQuantity FROM products WHERE stockQuantity < 5", function (err, res) {
+        //Loops through our the Result of the query for items with a stock quantity of less than 5 and lists the ID, Name, and Quantity of the item.
+        if (res.length > 0) {
+            for (var i = 0; i < res.length; i++) {
+                console.log("Product ID: " + res[i].id + " Product Name: " + res[i].productName + " Stock: " + res[i].stockQuantity + "\n");
+            };
+        } else {
+            console.log("All items currently meet Bamazon stock requirements\n");
+        }
+        //Returns user to the Manager Prompt.
+        managerPrompt();
+    })
+};
+
+
+var addInventory = function () {
+    //Connects to the Bamazon Database and requires the user to enter a number as valid product ID and Quantity.
+    connection.query("SELECT * FROM PRODUCTS", function (err, res) {
+        inquirer.prompt([{
+            name: "idReq",
+            type: "input",
+            message: "Enter the Product ID of the product you wish to restock",
+            validate: function (value) {
+                if (isNaN(value) === false) {
+                    return true;
+                } else {
+                    console.log(" is an invalid product ID.  Please try again.");
+                    return false;
+                }
+            }
+        },
+        {
+            name: "quantityReq",
+            type: "input",
+            message: "Enter a quantity to add",
+            validate: function (value) {
+                if (isNaN(value) === false) {
+                    return true;
+                } else {
+                    console.log(" is an invalid input.  Please try again by entering a number.");
+                    return false;
+                }
+            }
+        }
+        ]).then(function (answer) {
+            //Local variables that store the ID, Item Name, and quantity that is needed to update the products table.
+            var updateId;
+            var item;
+            var quantityUpdate;
+            //Validates that the product ID the user entered exists.  Restarts the prompt if it does not.
+            if (parseInt(answer.idReq) > res.length) {
+                console.log("Product ID " + answer.idReq + " does not exist.  Please enter a new Product ID.");
+                addInventory();
+            };
+            //Matches the ID the user entered with the ID's available in the products table and updates local variables.
+            for (var i = 0; i < res.length; i++) {
+                if (res[i].id === parseInt(answer.idReq)) {
+                    updateId = res[i].id;
+                    item = res[i].productName;
+                    quantityUpdate = res[i].stockQuantity + parseInt(answer.quantityReq);
+                }
+            };
+            //Re-establishes connection to the Bamazon Database to update the Products table with the new quantity.
+            connection.query("UPDATE products SET ? WHERE ?", [{ stockQuantity: quantityUpdate }, { id: updateId }], function (err, res) {
+                if (err) throw err;
+                //Provides user with a message summarizing the transaction.
+                console.log("\n" + item + " has been restocked.  The stock of this product is now " + quantityUpdate + "\n");
+                //Returns user to the Manager Prompt.
+                managerPrompt();
+            })
+        })
+    })
 };
